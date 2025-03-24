a4d26f3bc8c9a6e1ef5c52d65f346470b3ac74b5
Roger Reys <roger.reyesm97@gmail.com>
2025-03-23 22:16:21 -0500

    Configuracion btn set APT con variable no volatil stateInitSet

--- src/main.ino
+++ src/main.ino
@@ -4,11 +4,12 @@
 #include <SPIFFS.h>
 #include <DNSServer.h>
 #include <esp_sleep.h>
+#include <Preferences.h>
 
 // ESP32
-#define LEDS_PIN GPIO_NUM_3
-#define BTN_PIN_LED_PWR GPIO_NUM_2 // Pin del botón
-#define BTN_PIN_SET GPIO_NUM_1
+#define LEDS_PIN GPIO_NUM_15
+#define BTN_PIN_LED_PWR GPIO_NUM_4 // Pin del botón
+#define BTN_PIN_SET GPIO_NUM_5
 
 #define DNS_NAME "lamp.local"
 #define NUMPIXELS 50
@@ -19,6 +20,7 @@ WiFiManager wm;
 DNSServer dnsServer;
 WebServer server(80);
 Adafruit_NeoPixel pixels(NUMPIXELS, LEDS_PIN, NEO_GRB + NEO_KHZ800);
+Preferences preferences;
 
 // Variables globales para el color
 uint32_t colSkyBlue = pixels.Color(0, 200, 200);
@@ -182,6 +184,8 @@ void setup()
   // Configuracion Pixel
   pixels.begin();
   pixels.show();
+  // Iniciar la memoria no volátil
+  preferences.begin("storage", false);
 
   // Detectar si el ESP32 se despertó de deep sleep
   if (esp_sleep_get_wakeup_cause() == ESP_SLEEP_WAKEUP_EXT0)
@@ -198,54 +202,61 @@ void setup()
   // Configurar el GPIO para despertar cuando pase de HIGH -> LOW
   esp_sleep_enable_ext0_wakeup(BTN_PIN_LED_PWR, 0); // ESP32
 
-  // if (!SPIFFS.begin(true))
-  // {
-  //   Serial.println("Error al montar SPIFFS");
-  //   return;
-  // }
-  // Serial.println("SPIFFS montado correctamente");
+  if (!SPIFFS.begin(true))
+  {
+    Serial.println("Error al montar SPIFFS");
+    return;
+  }
+  Serial.println("SPIFFS montado correctamente");
+  Serial.printf("stateInitSet:'%s'\n",preferences.getBool("stateInitSet")?"true":"false");
+  if (preferences.getBool("stateInitSet", false))
+  {
+    // Configuracion punto de acceso a "LamparaIoT"
+    wm.setConfigPortalTimeout(180);       // Tiempo de espera en segundos
+    wm.setAPCallback(configModeCallback); // Callback cuando se inicia el AP
+    wm.setCustomHeadElement("<title>Configuración LamparaIoT</title>");
+    //  Intenta conectarse a una red guardada, si falla, inicia el portal de configuración
+    if (!wm.autoConnect("LamparaIoT"))
+    {
+      Serial.println("No se pudo conectar, reiniciando...");
+      delay(100);
+      preferences.putBool("stateInitSet", false);
+    }
+    if (WiFi.localIP())
+    {
+      // Si llega aquí, está conectado a la red WiFi
+      Serial.printf("Conectado a la red WiFi\nDirección IP:%s\n", WiFi.localIP().toString().c_str());
+      // Indicación visual de conexión exitosa
+      indicateColor(colGreen);
 
-  // Configuracion punto de acceso a "LamparaIoT"
-  // wm.setConfigPortalTimeout(180);       // Tiempo de espera en segundos
-  // wm.setAPCallback(configModeCallback); // Callback cuando se inicia el AP
-  // wm.setCustomHeadElement("<title>Configuración LamparaIoT</title>");
+      server.on("/", handleRoot);
+      server.on("/chroma.png", handleImage);
+      server.on("/script.min.js", handleScript);
+      server.on("/api", handleColor);
 
-  // Intenta conectarse a una red guardada, si falla, inicia el portal de configuración
-  // if (!wm.autoConnect("LamparaIoT"))
-  // {
-  //   Serial.println("No se pudo conectar, reiniciando...");
-  //   delay(100);
-  //   // ESP.restart();
-  // }
-  // if (WiFi.localIP())
-  // {
-  //   // Si llega aquí, está conectado a la red WiFi
-  //   Serial.printf("Conectado a la red WiFi\nDirección IP:%s\n", WiFi.localIP().toString().c_str());
-  //   // Indicación visual de conexión exitosa
-  //   indicateColor(colGreen);
+      server.begin();
 
-  //   server.on("/", handleRoot);
-  //   server.on("/chroma.png", handleImage);
-  //   server.on("/script.min.js", handleScript);
-  //   server.on("/api", handleColor);
-
-  //   server.begin();
-
-  //   // Inicia el servidor DNS
-  //   if (dnsServer.start(53, DNS_NAME, WiFi.localIP()))
-  //   {
-  //     Serial.printf("Servidor DNS iniciado correctamente: %s -> %s\n", WiFi.localIP().toString().c_str(), DNS_NAME);
-  //   }
-  //   else
-  //   {
-  //     Serial.println("Error al iniciar el servidor DNS");
-  //   }
-  // }
-  // else
-  // {
-  Serial.println("Sin conexion a la red");
-  indicateColor(colYellow);
-  // }
+      // Inicia el servidor DNS
+      if (dnsServer.start(53, DNS_NAME, WiFi.localIP()))
+      {
+        Serial.printf("Servidor DNS iniciado correctamente: %s -> %s\n", WiFi.localIP().toString().c_str(), DNS_NAME);
+      }
+      else
+      {
+        Serial.println("Error al iniciar el servidor DNS");
+      }
+    }
+    else
+    {
+      Serial.println("Sin conexion a la red");
+      indicateColor(colYellow);
+    }
+  }
+  else
+  {
+    Serial.println("Sin conexion a la red");
+    indicateColor(colYellow);
+  }
 }
 
 /***********************************
@@ -257,13 +268,11 @@ void loop()
   {
     // Procesa las solicitudes DNS
     dnsServer.processNextRequest();
-
     // Maneja las solicitudes HTTP
     server.handleClient();
   }
   // Detecta si se presionó el botón
   handleButtonPress();
-
   // Actualiza efectos sin bloquear el loop
   unsigned long currentMillis = millis();
 
@@ -319,10 +328,13 @@ void handleButtonPress()
     // Verificar si fue un clic corto o largo
     if (tiempoTotal < tiempoClicLargo)
     {
-      currentMode = (currentMode + 1) % 5; // Cambia de modo (0-4)
-      // Serial.printf("BTN modo: %d\n", currentMode);
-      lastDebounceTime = millis();
-      stepCounter = 0; // Reiniciar pasos para nuevos efectos
+      if (millis() - lastDebounceTime > debounceDelay)
+      {
+        currentMode = (currentMode + 1) % 5; // Cambia de modo (0-4)
+        // Serial.printf("BTN modo: %d\n", currentMode);
+        lastDebounceTime = millis();
+        stepCounter = 0; // Reiniciar pasos para nuevos efectos
+      }
     }
     else
     {
@@ -336,7 +348,9 @@ void handleButtonPress()
   }
   if (btnStatePinSet == LOW && lastbtnStatePinSet == HIGH)
   {
-    // SET
+    Serial.println("------ REINICIO POR BTN ------");
+    preferences.putBool("stateInitSet", true);
+    ESP.restart();
   }
   lastbtnStateLedPwr = btnStateLedPwr;
   lastbtnStatePinSet = btnStatePinSet;
